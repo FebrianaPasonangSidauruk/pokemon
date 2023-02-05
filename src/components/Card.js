f662a8a5e325c216758940fe4d1aefd87c293cfa
FebrianaPasonangSidauruk <febriana.pasonang@ui.ac.id>
2023-02-06 01:22:46 +0700

    update style

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,19 +5,12 @@ import axios from 'axios';
 const Card = ({pokemon}) => {
     const [idPokemon, setIdPokemon] = useState([]);
     const [show, setShow] = useState(false);
-    // console.log(pokemon)
 
     const getIdSelected= async(idPoke)=>{
       setShow(true);
-      // const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPoke}`);
-      // console.log(response.data);
-      // setIdPokemon(response.data);
-      // console.log(idPokemon)
       setIdPokemon(idPoke);
-      console.log(idPokemon);
     }
 
-    // console.log(idPokemon)
 
   return (
     <div>
@@ -27,10 +20,10 @@ const Card = ({pokemon}) => {
         {pokemon.map((item)=>(
             <div key={item.id} >
               <div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-                <img src={item.sprites.front_default} />
-                <h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
-                  {item.name} {item.id}
+              <h5 className='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+                  {item.name} 
                 </h5>
+                <img src={item.sprites.front_default} />
                 <div className="flex items-center justify-between">
                   <button onClick={()=> getIdSelected(item)}  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Detail
@@ -39,7 +32,9 @@ const Card = ({pokemon}) => {
               </div>
             </div>
         ))}
+        <div className=''>
         {show && <Detail setShow={setShow} idPokemon={idPokemon}/>}
+        </div>
         </div>
         </>
         }
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,96 +1,64 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Detail = ({setShow, idPokemon}) => {
-  const [ability, setAbility] = useState([]);
+const Detail = ({ setShow, idPokemon }) => {
   const [moves, setMoves] = useState([]);
-  var pokemon = ([]);
-  // console.log(idPokemon);
-  
-  // const [dataPoke, setDataPoke]=useState([]);
 
-  
-  const cek =()=>{
-    // console.log(idPokemon);
-    pokemon = idPokemon.abilities.map((item)=> item.ability.name);
-    setAbility(pokemon);
-    console.log(pokemon)
-
-    const movesList = idPokemon.moves.map(move=>move.move.name).slice(0, 10);
+  const cek = () => {
+    const movesList = idPokemon.moves.map(move => move.move.name).slice(0, 10);
     setMoves(movesList);
   }
 
-  console.log(moves);
-  console.log(ability);
-
-  useEffect(()=>{
+  useEffect(() => {
     cek();
-}, [idPokemon]);
-  // setDataPoke(idPokemon)
+  }, [idPokemon]);
+
   return (
-    <div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-blue-800 dark:border-blue-700'>
-      <button className='bg-red-800' onClick={()=>setShow(false)}>X</button>
+    <div className='fixed mx-auto sm:w-3/4 md:w-2/4 inset-x-0 top-40 z-50  w-full p-4 overflow-x-hidden overflow-y-auto  h-modal md:h-full'>
+      <div className='relative w-full h-full max-w-2xl md:h-auto'>
+    <div className=' border border-gray-200 rounded-lg shadow dark:bg-blue-800 dark:border-blue-700'>
+     <div className='flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600'>
+     <h1>{idPokemon.name}</h1>
+      <button className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white' onClick={() => setShow(false)}>
+        X
+      </button>
+      </div>
       {/* <h1>{idPokemon.name}</h1> */}
-      {/* for (let index = 0; index = pokemon.length; index++) {
-        const element = array[index];
-        
-      } */}
-      <div className=''>
+      <div className='flex items-center justify-between  text-white'>
+      <img src={idPokemon.sprites.front_default} />
+      <div className='flex flex-col px-4'>
         <li>
-        <span>
-          Types:
-          {
-            idPokemon.types.map((item)=>(
-              <span>{item.type.name}</span>
-            )).reduce((prev, curr) => (prev ? [' ', prev, ', ', curr] : curr), null)}
-        </span>
+          <span>Types:
+            {idPokemon.types.map((item) => (
+                <span>{item.type.name}</span>
+              )).reduce((prev, curr) => (prev ? [' ', prev, ', ', curr] : curr), null)}
+          </span>
         </li>
         <li><span>Weight: {idPokemon.weight} Hectograms</span></li>
         <li><span>Height: {idPokemon.height} Decimetres</span></li>
         <li>
-        <span>Abilities :  
-      {ability.map((item)=>(
-       <span>{item}</span>
-      )).reduce((prev, curr) => (prev ? [' ', prev, ', ', curr] : curr), null)}
-      </span> </li>
-      <li>
-        <span>10 Moves :  
-      {moves.map((item)=>(
-       <span>{item}</span>
-      )).reduce((prev, curr) => (prev ? [' ', prev, ', ', curr] : curr), null)}
-      </span> </li>
-      <li>
-        Stats:
-        {
-          idPokemon.stats.map((item)=>(
-            <p>{item.stat.name} : {item.base_stat}</p>
-          ))
-        }
-      </li>
+          <span>Abilities :
+            {idPokemon.abilities.map((item) => (
+              <span>{item.ability.name}</span>
+            )).reduce((prev, curr) => (prev ? [' ', prev, ', ', curr] : curr), null)}
+          </span> </li>
+        <li>
+          <span>10 Moves :
+            {moves.map((item) => (
+              <span>{item}</span>
+            )).reduce((prev, curr) => (prev ? [' ', prev, ', ', curr] : curr), null)}
+          </span> </li>
+        <li>
+          Stats:
+            {idPokemon.stats.map((item) => (
+              <p className='ml-5'> {item.stat.name} : {item.base_stat}</p>
+            ))
+          }
+        </li>
       </div>
-      {/* {pokemon.forEach(element=>{
-        console.log(element)
-      })} */}
-      {/* {cek} */}
-      {/* {
-        idPokemon.abilities.map((item)=>(
-          <p>{item.ability.name}</p>
-        ))
-      } */}
-
-      {/* {
-        pokemon.map(item=>
-          <>
-          console.log('ffa');
-          <p>{item}</p>
-          </>
-        )
-      } */}
-
-
-      {/* {
-        ability.map
-      } */}
       </div>
+      </div>
+    </div>
+    </div>
   )
 }
 
